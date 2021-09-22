80cf032be4b65f04ddcfac1478e64f34bac40a0a
Nathan Sprenkle <nsprenkle@edx.org>
2021-09-22 17:58:33 -0400

    feat: add routing for enhanced staff grader mocks

--- lms/djangoapps/ora_staff_grader/urls.py
+++ lms/djangoapps/ora_staff_grader/urls.py
@@ -1,15 +1,17 @@
 """
 URLs for Enhanced Staff Grader (ESG) backend-for-frontend (BFF)
 """
-
+from django.conf.urls import include, url
 from django.urls.conf import path
 
 from lms.djangoapps.ora_staff_grader.views import InitializeView, SubmissionFetchView
 
 
 urlpatterns = []
+app_name = "ora-staff-grader"
 
 urlpatterns += [
+    url('mock', include('lms.djangoapps.ora_staff_grader.mock.urls')),
     path(
         'initialize', InitializeView.as_view(), name='initialize'
     ),
