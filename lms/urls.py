3a1acf29840152fbb33215fff7a4355e76bce1d9
Dillon Dumesnil <ddumesnil@edx.org>
2021-11-10 12:19:45 -0500

    feat: AA-1055: Add in User Tours to the platform
    User Tours are walkthroughs we are able to give in our frontends.
    This sets up the backend support for them by creating the model,
    setting up the initial backfill, creating a signal listener to create
    a default UserTour on user registration, and sets up some endpoints
    to get user tour information and update it. It is also being
    initialized with a waffle flag to control the rollout. The flag is
    intended to control all tours and not allow for opting into only some tours.

--- lms/urls.py
+++ lms/urls.py
@@ -996,6 +996,11 @@ urlpatterns += [
     url(r'^api/course_home/v1/', include(('lms.djangoapps.course_home_api.urls', 'course-home-v1'))),
 ]
 
+# User Tour API urls
+urlpatterns += [
+    url(r'^api/user_tours/', include('lms.djangoapps.user_tours.urls')),
+]
+
 # Course Experience API urls
 urlpatterns += [
     url(r'^api/course_experience/', include('openedx.features.course_experience.api.v1.urls')),
